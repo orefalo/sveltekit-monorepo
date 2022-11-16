68023c88ae749f314bf8a5ad48aa03166e09fe58
Olivier Refalo <orefalo@snapcore.com>
2022-11-16 19:03:24 +0400

    prettier now in tooling

--- tooling/README.md
+++
@@ -1,3 +0,0 @@
-This `/packages` folder contains reusable modules that aren't full libraries or apps in themselves.
-
-Currently this is simply just limited to eslint and tsconfig used in `/app`.
---
+++ tooling/prettier-config-custom/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+	printWidth: 100,
+	semi: false,
+	singleQuote: true,
+	tabWidth: 4,
+	trailingComma: 'none',
+	useTabs: true,
+	pluginSearchDirs: ['.'],
+	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }]
+}
